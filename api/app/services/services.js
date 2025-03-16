23c933fb1502c3d1a0b69d72d69e2e407ee5a2ab
Osmancito1 <osma_barahonas@unicah.edu>
2025-03-15 22:04:47 -0600

    Avance del Proyecto

---
+++ api/app/services/services.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const jwt = require('jwt-simple');
+const moment = require('moment');
+
+function createToken(user){
+    const payload = {
+        sub: user.user_id,
+        iat: moment().unix(),
+        exp: moment().add(14, 'days').unix(),
+    };
+    return jwt.encode(payload, process.env.SECRET_TOKEN);
+}
+
+function decodeToken(token){
+    const decoded = new Promise((resolve, reject) => {
+        try {
+            const payload = jwt.decode(token, process.env.SECRET_TOKEN);
+            if(payload.exp <= moment().unix()){
+                reject({
+                    status: 401,
+                    message: 'El token ha expirado'
+                });
+            }
+            resolve(payload.sub);
+        } catch (error) {
+            reject({
+                status: 500,
+                message: 'Invalid Token',
+                errorMessage: error.message
+            });
+        }
+    });
+    return decoded;
+}
+
+module.exports = {
+    createToken,
+    decodeToken
+}
